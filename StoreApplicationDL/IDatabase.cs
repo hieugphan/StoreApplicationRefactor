671d29e76159b1925433a3f4f0f7e35815a222e5
hieugphan <hieu.g.phan@hotmail.com>
2021-09-14 20:39:51 -0500

    Created database interface

---
+++ StoreApplicationDL/IDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StoreApplicationDL
+{
+    public interface IDatabase<T> where T: class 
+    {
+        Task<T> Create(T model);
+        Task<T> Delete(T model);
+        Task<IList<T>> Query(QueryOptions<T> options);
+        Task<T> FindSingle(QueryOptions<T> options);
+        Task<T> Update(T model);
+        Task<bool> Save();
+    }
+}
