302312632ade325d4fe1ad701ccf133ebea38210
hieugphan <hieu.g.phan@hotmail.com>
2021-09-24 02:09:41 -0500

    added data annotation

--- StoreApplicationEntities/Customer.cs
+++ StoreApplicationEntities/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -24,8 +25,9 @@ namespace StoreApplicationEntities
         {
         }
 
+        [Key]
         public int Id { get => _id; set => _id = value; }
-
+        [Required]
         public string Email
         {
             get
